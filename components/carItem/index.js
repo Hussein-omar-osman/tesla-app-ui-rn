2cf868b3688c88c1c65df6d24086984063c8575b
Hussein Omar <husseinomar6190@gmail.com>
2022-07-02 11:13:39 +0300

    carItem componet

--- components/carItem/index.js
+++ components/carItem/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, View } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import styles from './styles';
 
 const CarItem = () => {
   return (
-    <View>
-      <></>
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require('../../assets/images/ModelS.jpeg')}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle}>Starting from $69,000</Text>
+      </View>
     </View>
   );
 };
